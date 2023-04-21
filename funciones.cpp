8900e3c97d37981728a49d75bad42b4084872402
Hanssell89 <hanssell.llanes16143@est.uca.edu.ni>
2023-04-21 08:25:44 -0600

    Inicio de prueba

--- funciones.cpp
+++ funciones.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
 using namespace std;
-#define MAX 10
+#define MAX 100
 
 //Arreglo
 int edades[MAX];
-int pos=0;//posicion del elemento
+int pos=0;  //posicion del elemento
 
 void agregarEdad(int edad);
 bool hayEspacio();
+void mostrarEdades();
+bool esMayor(int edad);
+void mostrarEdadesMayores();
+int menu();
 
 void agregarEdad(int edad){
     edades[pos] = edad;
@@ -17,4 +21,34 @@ void agregarEdad(int edad){
 
 bool hayEspacio(){
     return pos < MAX;
+}
+
+void mostrarEdades(){
+    for(int i = 0; i < pos; i++){
+        cout << edades[i] << endl;
+    }
+}
+
+bool esMayor(int edad){
+    return edad >= 18;
+}
+
+void mostrarEdadesMayores(){
+    for(int i = 0; i < pos; i++){
+        if (esMayor(edades[i]))
+        cout << edades[i] << endl;
+    }
+}
+
+int menu(){
+    int op;
+    system("cls");
+    cout << "Listado de edades" << endl;
+    cout << "1. Agregar edad\n";
+    cout << "2. Mostrar edades\n";
+    cout << "3. Mostrar edades mayores o iguales a 18\n";
+    cout << "4. Salir\n";
+    cout << "Digite la opcion: ";
+    cin >> op;
+    return op;
 }
